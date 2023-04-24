cb8cdd631c1708fd6ffece660c858560113a02a2
Abhijeet Singh <43774040+geekyAbhijeetSr@users.noreply.github.com>
2023-04-24 16:54:32 +0530

    show size in kilobytes without points/decimal

--- src/helper/utils.js
+++ src/helper/utils.js
@@ -4,7 +4,9 @@ export function bytesToSize(bytes) {
 	const i = Math.floor(Math.log(bytes) / Math.log(1024))
 	if (i === 0) return `${bytes} ${sizes[i]}`
 	const val = bytes / Math.pow(1024, i)
-	if (i <= 2) {
+	if (i == 1) {
+		return `${val.toFixed(0)} ${sizes[i]}`
+	} else if (i == 2) {
 		return `${val.toFixed(1)} ${sizes[i]}`
 	} else if (i <= 4) {
 		return `${val.toFixed(1)} ${sizes[i]}`
