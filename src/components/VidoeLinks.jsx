72fd742b2aeb309ad3d204a6e9e49f1622cdf8e2
Abhijeet Singh <43774040+geekyAbhijeetSr@users.noreply.github.com>
2023-04-24 14:25:36 +0530

    remove useless stuff

--- src/components/VidoeLinks.jsx
+++ src/components/VidoeLinks.jsx
@@ -10,18 +10,6 @@ import { Button, Stack, Typography } from '@mui/material'
 import { bytesToSize } from '../helper/utils'
 import { VolumeOffOutlined, VolumeUpOutlined } from '@mui/icons-material'
 
-function createData(name, calories, fat, carbs, protein) {
-	return { name, calories, fat, carbs, protein }
-}
-
-const rows = [
-	createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-	createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-	createData('Eclair', 262, 16.0, 24, 6.0),
-	createData('Cupcake', 305, 3.7, 67, 4.3),
-	createData('Gingerbread', 356, 16.0, 49, 3.9),
-]
-
 export default function VideoLinks({ videos }) {
     
     const downloadClickHandle = (url) => {
@@ -31,7 +19,8 @@ export default function VideoLinks({ videos }) {
         downloadLink.target = '_blank'
         downloadLink.click()
         downloadLink.remove()
-    }
+	}
+	
 	return (
 		<TableContainer component={Paper}>
 			<Table sx={{ minWidth: 100 }} aria-label='simple table'>
