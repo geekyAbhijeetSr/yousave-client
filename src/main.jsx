ae27584de8e70910f05ff62af2d101a20a199c55
Abhijeet Singh <43774040+geekyAbhijeetSr@users.noreply.github.com>
2024-07-11 16:59:01 +0530

    add alert message for delay caused by backend hosting service

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Toaster } from 'react-hot-toast'
 
+// message for delay in request caused by free hosting of backend
+setTimeout(() => {
+	alert(`I'm currently using a free hosting service for backend, free instance will spin down with inactivity, which can delay requests by 50 seconds or more, which means there might be a short delay when you first interact with the application. I apologize for any inconvenience this may cause.
+
+Thank you for your patience.`)
+}, 2000)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Toaster
